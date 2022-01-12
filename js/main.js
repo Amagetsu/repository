9ef528ebe74f5df0289ae7424d495a0c57315f2c
Amagetsu <gengrebenuck11@gmail.com>
2022-01-12 15:17:41 +0200

    DZ 3

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// 1)
+
+// const userNum1 = prompt('Введите первое число')
+// const userNum2 = prompt('Введите второе число')
+
+// if (userNum1 < userNum2 ) {
+//     alert('-1')
+// }else if (userNum1 > userNum2) {
+//     alert('1')
+// }else if (userNum1 = userNum2) {
+//     alert('0')
+// }
+
+// 2)
+
+// const userNum = prompt('Введите число для вычисления факториала');
+// function factorial(num){
+//   for(var i = num - 1; i > 0; i--){
+//        num *= i;
+//    }
+//    return num;     
+//  }
+ 
+//  console.log(factorial(userNum));
+// alert(factorial(userNum));
+
+// 3)
+
+// const userNum1 = prompt('Введите первое число')
+// const userNum2 = prompt('Введите второе число')
+// const userNum3 = prompt('Введите третье число')
+
+// function ls(userNum1, userNum2, userNum3) {
+//   return userNum1 + userNum2 + userNum3
+// }
+// alert(ls(userNum1, userNum2, userNum3))
+
+// 4)
+
+// alert('Если ввести только один параметр, посчитаеться площадь квадрата')
+// const width = prompt('Введите ширину')
+// const length = prompt('Введите длину')
+
+
+// function rectangle (width, length) {
+//   if (width, length) {
+//     return width * length
+//   }else if (width || length) {
+//     return width * width || length * length
+//   }
+// }
+// alert(rectangle (width, length))
+
+// for (let car of cars) {
+  
+// }
+
+// 5)
+
+// alert('Введите число для вычисления совершенного числа')
+// const sov = prompt('')
+
+// function extraPerfect(sov){
+//   var perfects = [];
+
+//   for(i=0; i<n; i++){
+//     var binary = toBinary(i);
+
+//     if(binary[0] === binary[binary.length]){
+//       perfects.push(i);
+//     }
+//   }
+
+//   return perfects;
+// }
+// alert(extraPerfect(sov))
+
+
+
+
+const num = prompt('Дай читабельный размер заголовку ')
+// num = Number(num)
+// console.log(num)
+
+
+if (200 > num > 10){
+    alert('чел харош');
+} else {
+    alert('Иди ');
+}
